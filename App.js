d9df21489f72250a3522ee1d6280fcd17d204455
Sajithmym <sajithmym@gmail.com>
2025-05-25 01:22:39 +0530

    pks

--- App.js
+++ App.js
@@ -1,2 +1,9 @@
-import App from './app/_layout';
-export default App;
+import { registerRootComponent } from 'expo';
+import { ExpoRoot } from 'expo-router';
+
+// Must be exported or Fast Refresh won't update the context
+export function App() {
+  return <ExpoRoot context={require.context('./app')} />;
+}
+
+registerRootComponent(App);
