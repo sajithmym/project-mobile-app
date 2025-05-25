363a80abf5a6ff11a221c4b9f657de2e8aa2bbc8
Sajithmym <sajithmym@gmail.com>
2025-05-25 22:05:04 +0530

    do

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
+import { store } from './src/store/store';
+import AppNavigator from './src/navigation/AppNavigator';
+import { ThemeProvider } from './context/ThemeContext';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <ThemeProvider>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </ThemeProvider>
+    </Provider>
+  );
+};
+
+export default App;
